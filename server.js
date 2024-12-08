d66e023a1fad031329b7afd6f22e9ff82aeaff10
intrafut-team <intrafutteam@gmail.com>
2024-12-08 22:11:37 +0330

    change database

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 const pool = new Pool({
   connectionString:
-    "postgresql://neondb_owner:xPguFQUopa17@ep-jolly-mud-a465h29g.us-east-1.aws.neon.tech/sport?sslmode=require",
+    "postgres://neondb_owner:TG0uAS6egQsz@ep-floral-rain-a2iq8paf-pooler.eu-central-1.aws.neon.tech/sport?sslmode=require",
   ssl: {
     rejectUnauthorized: false,
   },
